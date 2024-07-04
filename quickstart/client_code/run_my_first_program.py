8a5aa03c0bf263002de254e67423ab33fe090a27
Krishnav1237 <krishnavkanoi2005@gmail.com>
2024-07-04 19:38:02 +0000

    my new nada program

--- quickstart/client_code/run_my_first_program.py
+++ quickstart/client_code/run_my_first_program.py
@@ -1,16 +1,27 @@
 from nada_dsl import *
 
 def nada_main():
-    # ... Define parties (bidders)
+    party1 = Party(name="Party1")
+    party2 = Party(name="Party2")
 
-    bids = [SecretInteger(Input(name=f"Bid{i}", party=party)) for i, party in enumerate(parties)]
+    data1 = SecretList(Input(name="Data1", party=party1), SecretInteger)
+    data2 = SecretList(Input(name="Data2", party=party2), SecretInteger)
 
-    highest_bid = bids[0]
-    winner = parties[0]
+    total_sum = sum(data1) + sum(data2) # ... Add sums from other parties
 
-    for i in range(1, len(bids)):
-        if bids[i] > highest_bid:
-            highest_bid = bids[i]
-            winner = parties[i]
+    total_count = len(data1) + len(data2) # ... Add counts from other parties
 
-    return [Output(winner, "winner"), Output(highest_bid, "winning_bid")]
+    mean = total_sum / total_count
+
+    squared_diffs_sum = sum((x - mean)**2 for x in data1) 
+    squared_diffs_sum += sum((x - mean)**2 for x in data2)
+
+    variance = squared_diffs_sum / total_count
+
+    std_dev = variance ** 0.5
+
+    return [
+        Output(mean, "Mean"),
+        Output(variance, "Variance"),
+        Output(std_dev, "Standard Deviation")
+    ]
